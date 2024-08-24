8c4efd40efcd55c1c83f4c142992ae5fb7efb4f0
ViniciusGomesc <kvyncy@outlook.com>
2024-08-24 18:10:43 -0300

    Atualizações postgres
    ^^

--- backend/app/database/Conection.py
+++ backend/app/database/Conection.py
@@ -4,11 +4,11 @@ import psycopg2
 # Conexão com o PostgreSQL
 def get_db():
     conn = psycopg2.connect(
-        dbname="your_database_name",
-        user="your_username",
-        password="your_password",
-        host="your_host",
-        port="your_port"
+        dbname="CalvoBank_Database",
+        user="user",
+        password="password",
+        host="localhost",
+        port="5432"
     )
     try:
         yield conn
